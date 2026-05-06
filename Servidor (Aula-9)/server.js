ab6aa3c80e128b258baf68dcabc396aa03b21cd7
ch <carloshenriqueharada@gmail.com>
2026-05-06 16:15:26 -0300

    push commit aula 11

--- Servidor (Aula-9)/server.js
+++ Servidor (Aula-9)/server.js
@@ -4,6 +4,16 @@ var http = require("http");
 var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
+var mongodb = require("mongodb");
+
+const MongoClient = mongodb.MongoClient;
+
+const uri = "mongodb+srv://dbUser:SenhaBolada@cluster0.v1dast6.mongodb.net/?appName=Cluster0"
+
+const client = new MongoClient(uri, { useNewUrlParser: true });
+
+var dbo = client.db("exemplo_bd");
+var usuarios = dbo.collection("usuarios");
 
 app.set("views", "./Views");
 app.set("view engine", "ejs");
@@ -66,3 +76,32 @@ app.get("/for", function(req, res){
     console.log(qtde)
 
 })
+
+app.post("/cadastrar_user", function(req,res){
+    var data = { dbnome: req.body.nome_bd, dblogin: req.body.login_bd, dbsenha: req.body.senha_bd};
+
+    usuarios.insertOne(data, function(err){
+        if (err) {
+            res.render("resposta_usuario.ejs", {resposta: "Erro ao cadastrar usuário!"})
+        }else{
+            res.render("resposta_usuario.ejs", {resposta: "Usuário cadastrado com sucesso!"});
+        };
+    });
+
+});
+
+app.post("/logar_user", function(req,res){
+    var data = { dblogin: req.body.login_bd, dbsenha: req.body.senha_bd};
+
+    usuarios.find(data).toArray(function(err, items){
+        console.log(items);
+        if(items.length == 0) {
+            res.render("resposta_usuario", {resposta: "Usuário/Senha não encontrados!"})
+        } else if (err){
+            res.render("resposta_usuario", {resposta: "Erro ao logar usuário"})
+        } else {
+            res.render("resposta_usuario", {resposta: "Usuário logado com sucesso!"})
+        };
+    });
+
+});
